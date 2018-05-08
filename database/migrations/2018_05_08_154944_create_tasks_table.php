cca9f049254fbf86e548f59e35f6965a813162fb
EC2 Default User <ec2-user@ip-172-31-15-41.us-east-2.compute.internal>
2018-05-08 12:04:36 +0000

    “changes”

--- database/migrations/2018_05_08_154944_create_tasks_table.php
+++ database/migrations/2018_05_08_154944_create_tasks_table.php
@@ -13,14 +13,14 @@ class CreateTasksTable extends Migration
      */
     public function up()
     {
-        Schema::create('tasks', function (Blueprint $table) {
-            $table->increments('id');
-            $table->integer('user_id')->unsigned()->index();
-            $table->string('content');
-            $table->timestamps();
+        //Schema::create('tasks', function (Blueprint $table) {
+            //$table->increments('id');
+            //$table->integer('user_id')->unsigned()->index();
+            //$table->string('content');
+            //$table->timestamps();
             
-            $table->foreign('user_id')->references('id')->on('users');
-        });
+            //$table->foreign('user_id')->references('id')->on('users');
+        //});
     }
 
     /**
